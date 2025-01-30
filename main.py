6c709bc969d67f2c0148524c1db6426092802290
Abednego Tenge <abednegotenge180@gmail.com>
2025-01-30 20:20:14 +0000

    last touching

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/contact", method=['GET', 'POST'])
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
     if request.method == "POST":
         email = request.form.get('email')
         message = request.form.get('message')
         subject = request.form.get('subject')
-        send_message = f"Subject: {subject}/n/n{message}"
+        send_message = f"Subject: {subject}\n\n{message}"
         try:
             with SMTP("smtp.gmail.com", port=587) as connection:
                 connection.starttls()
                 connection.login(EMAIL, PASSWORD)
-                connection.send_message(from_addr=EMAIL,
-                                        to_addrs=email,
-                                        msg=send_message
-                                        )
+                connection.sendmail(from_addr=EMAIL,
+                                    to_addrs=email,
+                                    msg=send_message)
+                print("Email sent successfully")
         except Exception as e:
-            print(e)
+            print(f"Error sending email: {e}")
     return render_template('contact.html')
 
 
