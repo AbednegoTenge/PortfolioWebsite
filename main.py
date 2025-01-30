d861b511674cb4afd91295b41dc3269ef9a53e48
Abednego Tenge <abednegotenge180@gmail.com>
2025-01-30 16:35:27 +0000

    finalising the about page

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from smtplib import SMTP
+import os
 
 app = Flask(__name__)
+
+EMAIL = os.environ.get('email')
+PASSWORD = os.environ.get('password')
 
 @app.route("/")
 def home():
@@ -10,8 +15,23 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/contact")
+@app.route("/contact", method=['GET', 'POST'])
 def contact():
+    if request.method == "POST":
+        email = request.form.get('email')
+        message = request.form.get('message')
+        subject = request.form.get('subject')
+        send_message = f"Subject: {subject}/n/n{message}"
+        try:
+            with SMTP("smtp.gmail.com", port=587) as connection:
+                connection.starttls()
+                connection.login(EMAIL, PASSWORD)
+                connection.send_message(from_addr=EMAIL,
+                                        to_addrs=email,
+                                        msg=send_message
+                                        )
+        except Exception as e:
+            print(e)
     return render_template('contact.html')
 
 
